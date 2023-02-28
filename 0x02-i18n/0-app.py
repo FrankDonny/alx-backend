34512ca8266c643733a59a382aaa914db455777f
frankdonny <arkohfrank1@gmail.com>
2023-02-28 23:07:30 +0000

    0x02. i18n

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""0-app module for i18n"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """index page"""
+    return render_template('0-index.html')
