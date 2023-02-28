c134723bdf346574d21144c123d6bc9c08e54615
frankdonny <arkohfrank1@gmail.com>
2023-02-28 23:55:31 +0000

    0x02. i18n

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """index page"""
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
