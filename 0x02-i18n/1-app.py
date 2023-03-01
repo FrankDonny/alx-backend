c681ba3fad97c034126b4d34e798ebfca7f86ad2
frankdonny <arkohfrank1@gmail.com>
2023-03-01 00:17:35 +0000

    0x02. i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,18 @@
+#!/usr/bin/env python3
+"""1-app module for i18n"""
 from flask import Flask
 from flask_babel import Babel
 
 app = Flask(__name__)
-# ...
+
 babel = Babel(app)
+
+
+class Config:
+    """configuration class for babel defaults"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
