3f71b48e0e728c3f85e316d61d127edb2bb26029
frankdonny <arkohfrank1@gmail.com>
2023-03-01 00:19:00 +0000

    0x02. i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """1-app module for i18n"""
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -16,3 +16,13 @@ class Config:
 
 
 app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
